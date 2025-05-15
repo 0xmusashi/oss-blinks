5ada9b363e9196ce1da71b45cda2f5313e65f243
0xnetero <0xnetero201@gmail.com>
2025-05-15 15:07:06 +0700

    fetch github repo info & switch to donate spl instead of sol

---
+++ src/app/api/actions/donate/route.ts
@@ -0,0 +1,318 @@
+import {
+    ActionGetResponse,
+    ActionPostRequest,
+    ActionPostResponse,
+    ACTIONS_CORS_HEADERS,
+    BLOCKCHAIN_IDS,
+    createPostResponse,
+} from "@solana/actions";
+
+import {
+    clusterApiUrl,
+    Connection,
+    PublicKey,
+    LAMPORTS_PER_SOL,
+    SystemProgram,
+    TransactionMessage,
+    VersionedTransaction,
+    Transaction,
+} from "@solana/web3.js";
+
+import * as splToken from '@solana/spl-token';
+
+import axios from 'axios';
+
+const SOLANA_DEVNET_USDC_PUBKEY =
+    'EKLbfTVy38gobhwWF18Kcosiwz4JViWwmA2fYrEKwaoj';
+
+// CAIP-2 format for Solana
+const blockchain = BLOCKCHAIN_IDS.devnet;
+
+// Create a connection to the Solana blockchain
+const connection = new Connection("https://api.devnet.solana.com");
+
+// Create headers with CAIP blockchain ID
+const headers = {
+    ...ACTIONS_CORS_HEADERS,
+    "x-blockchain-ids": blockchain,
+    "x-action-version": "2.4",
+};
+
+// OPTIONS endpoint is required for CORS preflight requests
+// Your Blink won't render if you don't add this
+export const OPTIONS = async () => {
+    return new Response(null, { headers });
+};
+
+// GET endpoint returns the Blink metadata (JSON) and UI configuration
+export const GET = async (req: Request) => {
+    try {
+        const requestUrl = new URL(req.url);
+        const { toPubkey, repo, repoName, avatarUrl } = await validatedQueryParams(requestUrl);
+
+        const baseHref = new URL(
+            `/api/actions/donate?to=${toPubkey.toBase58()}&repo=${repo}`,
+            requestUrl.origin,
+        ).toString();
+
+        const payload: ActionGetResponse = {
+            type: 'action',
+            title: `Donate USDC to ${repoName}`,
+            icon: avatarUrl,
+            description:
+                'Cybersecurity Enthusiast | Support my research with a donation.',
+            label: 'Donate', // this value will be ignored since `links.actions` exists
+            links: {
+                actions: [
+                    {
+                        label: 'Send 10 USDC', // button text
+                        href: `${baseHref}&amount=${'10'}`,
+                        type: "transaction"
+                    },
+                    {
+                        label: 'Send 50 USDC', // button text
+                        href: `${baseHref}&amount=${'50'}`,
+                        type: "transaction"
+                    },
+                    {
+                        label: 'Send 100 USDC', // button text
+                        href: `${baseHref}&amount=${'100'}`,
+                        type: "transaction"
+                    },
+                    {
+                        label: 'Send USDC', // button text
+                        href: `${baseHref}&amount={amount}`, // this href will have a text input
+                        parameters: [
+                            {
+                                name: 'amount', // parameter name in the `href` above
+                                label: 'Enter the amount of USDC to send', // placeholder of the text input
+                                required: true,
+                            },
+                        ],
+                        type: "transaction"
+                    },
+                ],
+            },
+        };
+
+        return Response.json(payload, {
+            headers,
+        });
+    } catch (err) {
+        console.log(err);
+        let message = 'An unknown error occurred';
+        if (typeof err == 'string') message = err;
+        return new Response(message, {
+            status: 400,
+            headers,
+        });
+    }
+};
+
+// POST endpoint handles the actual transaction creation
+export const POST = async (req: Request) => {
+    try {
+        const requestUrl = new URL(req.url);
+        const { amount, toPubkey } = await validatedQueryParams(requestUrl);
+
+        const body: ActionPostRequest = await req.json();
+
+        // validate the client provided input
+        let account: PublicKey;
+        try {
+            account = new PublicKey(body.account);
+        } catch (err) {
+            return new Response('Invalid "account" provided', {
+                status: 400,
+                headers,
+            });
+        }
+
+        const decimals = 9;
+        const mintAddress = new PublicKey(SOLANA_DEVNET_USDC_PUBKEY); // replace this with any SPL token mint address
+
+        // converting value to fractional units
+
+        let transferAmount: any = parseFloat(amount.toString());
+        transferAmount = transferAmount.toFixed(decimals);
+        transferAmount = transferAmount * Math.pow(10, decimals);
+
+        const fromTokenAccount = await splToken.getAssociatedTokenAddress(
+            mintAddress,
+            account,
+            false,
+            splToken.TOKEN_PROGRAM_ID,
+            splToken.ASSOCIATED_TOKEN_PROGRAM_ID,
+        );
+
+        let toTokenAccount = await splToken.getAssociatedTokenAddress(
+            mintAddress,
+            toPubkey,
+            true,
+            splToken.TOKEN_PROGRAM_ID,
+            splToken.ASSOCIATED_TOKEN_PROGRAM_ID,
+        );
+
+        const ifexists = await connection.getAccountInfo(toTokenAccount);
+
+        let instructions = [];
+
+        if (!ifexists || !ifexists.data) {
+            let createATAiX = splToken.createAssociatedTokenAccountInstruction(
+                account,
+                toTokenAccount,
+                toPubkey,
+                mintAddress,
+                splToken.TOKEN_PROGRAM_ID,
+                splToken.ASSOCIATED_TOKEN_PROGRAM_ID,
+            );
+            instructions.push(createATAiX);
+        }
+
+        let transferInstruction = splToken.createTransferInstruction(
+            fromTokenAccount,
+            toTokenAccount,
+            account,
+            transferAmount,
+        );
+        instructions.push(transferInstruction);
+
+        const transaction = new Transaction();
+        transaction.feePayer = account;
+
+        transaction.add(...instructions);
+
+        // set the end user as the fee payer
+        transaction.feePayer = account;
+
+        transaction.recentBlockhash = (
+            await connection.getLatestBlockhash()
+        ).blockhash;
+
+        const payload: ActionPostResponse = await createPostResponse({
+            fields: {
+                transaction,
+                message: `Donated ${amount} USDC to ${toPubkey.toBase58()}`,
+                type: 'transaction'
+            },
+            // note: no additional signers are needed
+            // signers: [],
+        });
+
+        return Response.json(payload, {
+            headers,
+        });
+    } catch (err) {
+        console.log(err);
+        let message = 'An unknown error occurred';
+        if (typeof err == 'string') message = err;
+        return new Response(message, {
+            status: 400,
+            headers,
+        });
+    }
+};
+
+const prepareTransaction = async (
+    connection: Connection,
+    payer: PublicKey,
+    receiver: PublicKey,
+    amount: number
+) => {
+    // Create a transfer instruction
+    const instruction = SystemProgram.transfer({
+        fromPubkey: payer,
+        toPubkey: new PublicKey(receiver),
+        lamports: amount * LAMPORTS_PER_SOL,
+    });
+
+    // Get the latest blockhash
+    const { blockhash } = await connection.getLatestBlockhash();
+
+    // Create a transaction message
+    const message = new TransactionMessage({
+        payerKey: payer,
+        recentBlockhash: blockhash,
+        instructions: [instruction],
+    }).compileToV0Message();
+
+    // Create and return a versioned transaction
+    return new VersionedTransaction(message);
+};
+
+const extractAvatar = async (repoUrl: string) => {
+    try {
+        const trimmedUrl = repoUrl.trim();
+        if (!trimmedUrl.startsWith('https://github.com/')) {
+            throw new Error('Invalid GitHub URL. Must start with https://github.com/');
+        }
+
+        const urlParts = new URL(trimmedUrl).pathname.split('/').filter(Boolean);
+        if (urlParts.length < 2) {
+            throw new Error('Invalid GitHub URL.  Must be a repo URL.');
+        }
+        const owner = urlParts[0];
+        const repoName = urlParts[0] + '/' + urlParts[1];
+
+        const userResponse = await axios.get(`https://api.github.com/users/${owner}`);
+        const avatarUrl = userResponse.data.avatar_url;
+
+        return {
+            avatarUrl,
+            repoName,
+        };
+    } catch (e: any) {
+        return {
+            avatarUrl: '',
+            repoName: '',
+        }; // Return empty string on error to avoid further issues.
+    }
+}
+
+const validatedQueryParams = async (requestUrl: URL) => {
+    let toPubkey: PublicKey = new PublicKey(
+        '2grKcZPjxKbNKkc8S6nmrSXtXTXJih4utVMa7jBwRcFf',
+    );
+    let amount: number = 10;
+    let repo: string = 'https://github.com/0xnetero/git-blinks';
+    let repoName: string = '0xNetero/git-blinks';
+    let avatarUrl: string = 'https://avatars.githubusercontent.com/u/203130627?v=4';
+
+    try {
+        if (requestUrl.searchParams.get('to')) {
+            toPubkey = new PublicKey(requestUrl.searchParams.get('to')!);
+        }
+    } catch (err) {
+        throw 'Invalid input query parameter: to';
+    }
+
+    try {
+        if (requestUrl.searchParams.get('amount')) {
+            amount = parseFloat(requestUrl.searchParams.get('amount')!);
+        }
+
+        if (amount <= 0) throw 'amount is too small';
+    } catch (err) {
+        throw 'Invalid input query parameter: amount';
+    }
+
+    try {
+        if (requestUrl.searchParams.get('repo')) {
+            repo = requestUrl.searchParams.get('repo')!;
+            const extractedData = await extractAvatar(repo);
+            avatarUrl = extractedData.avatarUrl;
+            repoName = extractedData.repoName;
+        }
+
+    } catch (err) {
+        throw 'Invalid input query parameter: amount';
+    }
+
+    return {
+        amount,
+        toPubkey,
+        repo,
+        repoName,
+        avatarUrl,
+    };
+}
